402266f531b7cf9c909453772ab10bc8b398baa2
Ron Jeffries <ronjeffries@acm.org>
2022-08-17 12:06:46 -0400

    Tidy Game

--- src/main/kotlin/com/ronjeffries/adventureFour/Game.kt
+++ src/main/kotlin/com/ronjeffries/adventureFour/Game.kt
@@ -1,9 +1,9 @@
 package com.ronjeffries.adventureFour
 
 class Game(val world: World, startingName: String) {
-    val roomReferences: Set<String> get() = world.roomReferences
     var currentRoom = world.unsafeRoomNamed(startingName)
     val currentRoomName get() = currentRoom.name
+    val roomReferences: Set<String> get() = world.roomReferences
 
     fun command(cmd: String) {
         when(cmd) {
