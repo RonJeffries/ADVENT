3cedb06d6be764404100e0a1a17f7fb00512cc21
Ron Jeffries <ronjeffries@acm.org>
2022-08-19 09:40:01 -0400

    Improved the window immensely

--- src/main/kotlin/com/ronjeffries/adventureFour/HelloApplication.kt
+++ src/main/kotlin/com/ronjeffries/adventureFour/HelloApplication.kt
@@ -4,14 +4,27 @@ import javafx.application.Application
 import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
 import javafx.scene.Scene
+import javafx.scene.control.Label
 import javafx.stage.Stage
-import tornadofx.App
-import tornadofx.View
-import tornadofx.launch
-import tornadofx.pane
+import tornadofx.*
+import tornadofx.Stylesheet.Companion.label
+
+var myLabel: Label by singleAssign()
 
 class MainView: View() {
-    override val root: Parent = pane()
+    override val root: Parent = vbox {
+        minWidth = 400.0
+        minHeight = 200.0
+        myLabel = label("Hello Ron")
+        button("Press me") {
+            action { someonePressedMe() }
+        }
+    }
+}
+
+fun someonePressedMe() {
+    println("pressed")
+    myLabel.text = "I saw that!"
 }
 
 class HelloApplication : App(MainView::class) {
