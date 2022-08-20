ea4b1097ad95593c5199c21c96b2949a5e5af3f1
Ron Jeffries <ronjeffries@acm.org>
2022-08-20 14:11:05 -0400

    initial game plays as intended.

--- src/main/kotlin/com/ronjeffries/adventureFour/HelloApplication.kt
+++ src/main/kotlin/com/ronjeffries/adventureFour/HelloApplication.kt
@@ -11,26 +11,43 @@ var count = 1
 var textContents = "Welcome to Tiny Adventure!"
 
 class MainView: View() {
+    val world = world {
+        room("wellhouse") {
+            desc("You're at wellhouse", "You're in a charming wellhouse")
+            go("n", "wellhouse")
+            go("s", "clearing")
+        }
+        room("clearing") {
+            desc("You're in a clearing", "You're in a charming clearing")
+            go("n", "wellhouse")
+            go("s","clearing")
+        }
+    }
+    val game = Game(world, "wellhouse")
+
     override val root: Parent = vbox {
         minWidth = 400.0
         minHeight = 200.0
-        myText = textarea(textContents) {
+        myText = textarea(textContents + "\n"+game.currentRoom.longDesc) {
             isEditable = false
         }
-        myCommand = textfield ("> ") {
+        myCommand = textfield ("") {
             action { someoneTyped() }
         }
     }
+    fun someoneTyped() {
+        val cmd = myCommand.text
+        game.command(cmd)
+        val newLine = "\n"+game.currentRoom.longDesc
+        myText.appendText("\n" + cmd)
+        myText.appendText(newLine)
+        myCommand.text = ""
+        myCommand.appendText("")
+        count++
+    }
 }
 
-fun someoneTyped() {
-    var newLine = "\nThat was command $count"
-    myText.appendText("\n" + myCommand.text)
-    myText.appendText(newLine)
-    myCommand.text = "> "
-    myCommand.appendText("")
-    count++
-}
+
 
 class HelloApplication : App(MainView::class) {
 }
