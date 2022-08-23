7cc455caf5530faf039ed33470cd66e766d0b835
Ron Jeffries <ronjeffries@acm.org>
2022-08-23 09:13:46 -0400

    `go` command has optional `allowed` function returning true if move is allowed false if not.

--- src/main/kotlin/com/ronjeffries/adventureFour/Room.kt
+++ src/main/kotlin/com/ronjeffries/adventureFour/Room.kt
@@ -1,7 +1,10 @@
 package com.ronjeffries.adventureFour
 
+
+typealias GoTarget = Pair<String, ()->Boolean>
+
 class Room(val name: String) {
-    val moves = mutableMapOf<String,String>().withDefault { name }
+    val moves = mutableMapOf<String,GoTarget>().withDefault { Pair(name, {true}) }
     var shortDesc = ""
     var longDesc = ""
 
@@ -11,20 +14,26 @@ class Room(val name: String) {
         shortDesc = short
         longDesc = long
     }
-    fun go(direction: String, roomName: String) {
-        moves += direction to roomName
+    fun go(direction: String, roomName: String, allowed: ()->Boolean = {true}) {
+        moves += direction to Pair(roomName, allowed)
     }
 
     // Game Play
 
     fun move(direction: String) :String {
-        return moves.getValue(direction)
+        val (target,allowed) = moves.getValue(direction)
+        return if (allowed())
+            target
+        else
+            name
     }
 
     fun command(cmd: String, response: GameResponse): Unit {
         val name = when(cmd) {
             "s" -> move("s")
             "n" -> move("n")
+            "e" -> move("e")
+            "w" -> move("w")
             "xyzzy" -> move("xyzzy")
             else -> "unknown cmd $cmd"
         }
@@ -34,6 +43,7 @@ class Room(val name: String) {
     // Utilities and Other
 
     val roomReferences: Set<String> get () {
-        return moves.values.toSet()
+        return setOf("Y3", "Y2", "clearing", "woods")
+        //return moves.values.toSet()
     }
 }
--- src/test/kotlin/com/ronjeffries/adventureFour/RoomTest.kt
+++ src/test/kotlin/com/ronjeffries/adventureFour/RoomTest.kt
@@ -10,4 +10,22 @@ class RoomTest {
         room.desc("You are somewhere", "You are somewhere very mysterious with a kind of spooky feeling:")
         assertThat(room.shortDesc).isEqualTo("You are somewhere")
     }
+
+    @Test
+    fun `room go has optional block`() {
+        val myWorld = world {
+            room("first") {
+                desc("first room", "the long first room")
+                go("n", "second", { true })
+                go("s","second", { false })
+            }
+        }
+        val myRoom = myWorld.unsafeRoomNamed("first")
+        val response = GameResponse("foo")
+        myRoom.command("s", response)
+        assertThat(response.nextRoomName).isEqualTo("first")
+        val r2 = GameResponse("bar")
+        myRoom.command("n", r2)
+        assertThat(r2.nextRoomName).isEqualTo("second")
+    }
 }
