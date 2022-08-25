aa288ac2bb349b3f9e510ad04440bb1d8809a75e
Ron Jeffries <ronjeffries@acm.org>
2022-08-25 16:05:07 -0400

    Status objects moved to main/StatusObjects.kt.

---
+++ src/main/kotlin/com/ronjeffries/adventureFour/StatusObjects.kt
@@ -0,0 +1,22 @@
+package com.ronjeffries.adventureFour
+
+class BooleanStatusMap {
+    val map = mutableMapOf<String, BooleanStatus>()
+
+    fun item(name:String): BooleanStatus {
+        return map.getOrPut(name) { BooleanStatus() }
+    }
+}
+
+class BooleanStatus(var value: Boolean = false) {
+    val isTrue: Boolean get() = value
+    val isFalse: Boolean get() = !value
+
+    fun not(): Unit {
+        value = !value
+    }
+
+    fun set(truth: Boolean) {
+        value = truth
+    }
+}
--- src/test/kotlin/com/ronjeffries/adventureFour/StatusTest.kt
+++ src/test/kotlin/com/ronjeffries/adventureFour/StatusTest.kt
@@ -7,45 +7,20 @@ class StatusTest {
     @Test
     fun `status isTrue`() {
         val status = BooleanStatus()
-        assertThat(status.isTrue()).isEqualTo(false)
+        assertThat(status.isTrue).isEqualTo(false)
         status.set(true)
-        assertThat(status.isTrue()).isEqualTo(true)
+        assertThat(status.isTrue).isEqualTo(true)
+        assertThat(status.isFalse).isEqualTo(false)
 
     }
 
     @Test
     fun `Status map`() {
-        StatusMap().apply {
-            assertThat(item("unlocked").isFalse()).isEqualTo(true)
+        BooleanStatusMap().apply {
+            assertThat(item("unlocked").isFalse).isEqualTo(true)
             item("unlocked").not()
-            assertThat(item("unlocked").isTrue()).isEqualTo(true)
+            assertThat(item("unlocked").isTrue).isEqualTo(true)
         }
     }
 }
 
-class StatusMap {
-    val map = mutableMapOf<String, BooleanStatus>()
-
-    fun item(name:String): BooleanStatus {
-        return map.getOrPut(name) { BooleanStatus() }
-    }
-}
-
-class BooleanStatus(var value: Boolean = false) {
-
-    fun set(truth: Boolean) {
-        value = truth
-    }
-
-    fun isTrue(): Boolean {
-        return value
-    }
-
-    fun isFalse(): Boolean {
-        return !value
-    }
-
-    fun not(): Unit {
-        value = !value
-    }
-}
