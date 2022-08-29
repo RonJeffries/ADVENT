24dec45912ae67a3a4c748f9b822b82a6ffbbc2c
Ron Jeffries <ronjeffries@acm.org>
2022-08-29 09:05:54 -0400

    CommandExperiment understands directions and magic words, plus take, go, say.

--- src/test/kotlin/com/ronjeffries/adventureFour/CommandExperiment.kt
+++ src/test/kotlin/com/ronjeffries/adventureFour/CommandExperiment.kt
@@ -25,12 +25,45 @@ class CommandExperiment {
         assertThat(result).isEqualTo("went east.")
     }
 
-    fun `single word commands`() {
+    @Test
+    fun `expand magic`() {
+        val command = Command("xyzzy")
+        command.makeWords()
+        assertThat(command.words.size).isEqualTo(1)
+        command.expandIfNeeded()
+        assertThat(command.words.size).isEqualTo(2)
+        assertThat(command.words[0]).isEqualTo("say")
+        assertThat(command.words[1]).isEqualTo("xyzzy")
+        command.makeVerbNoun()
+        assertThat(command.verb).isEqualTo("say")
+        assertThat(command.noun).isEqualTo("xyzzy")
+    }
+
+    @Test
+    fun `single word go commands`() {
         val command = Command("east")
         val result = command
             .validate()
             .execute()
         assertThat(result).isEqualTo("went east.")
+    }
+
+    @Test
+    fun `single magic word commands`() {
+        val command = Command("xyzzy")
+        val result = command
+            .validate()
+            .execute()
+        assertThat(result).isEqualTo("said xyzzy.")
+    }
+
+    @Test
+    fun `single unknown word commands`() {
+        val command = Command("fragglerats")
+        val result = command
+            .validate()
+            .execute()
+        assertThat(result).isEqualTo("I don't understand fragglerats.")
     }
 }
 
@@ -66,10 +99,16 @@ private class Command(val input: String) {
             "n","e","s","w","north","east","south","west",
             "nw","northwest", "sw","southwest", "ne", "northeast", "se", "southeast",
             "up","dn","down")
+
+        val magicWords = listOf("xyzzy", "plugh")
         val word = words[0]
         if (word in directions) {
-            words[1] = words[0]
-            words[0] = "go"
+            words.add(0,"go")
+        } else if (word in magicWords) {
+            words.add(0,"say")
+        } else {
+            words[0] = "verbError"
+            words.add(word)
         }
         return this
     }
@@ -80,11 +119,23 @@ private class Command(val input: String) {
             operation = ::take
         else if (verb == "go")
             operation = ::go
+        else if (verb == "say")
+            operation = ::say
+        else if (verb == "verbError")
+            operation = ::verbError
         return this
+    }
+
+    fun verbError(noun: String) {
+        result = "I don't understand $noun."
     }
 
     fun go(noun: String) {
         result = "went $noun."
+    }
+
+    fun say(noun:String) {
+        result = "said $noun."
     }
 
     fun take(noun:String) {
