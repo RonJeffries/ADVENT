c634af1f705d989f341c81ef5c547934428a5962
Ron Jeffries <ronjeffries@acm.org>
2022-08-30 08:43:25 -0400

    Move command, public, to main folder.

---
+++ src/main/kotlin/com/ronjeffries/adventureFour/Command.kt
@@ -0,0 +1,84 @@
+package com.ronjeffries.adventureFour
+
+class Command(val input: String) {
+    val words = mutableListOf<String>()
+    var operation = this::commandError
+    var result: String = ""
+    val verb get() = words[0]
+    val noun get() = words[1]
+
+    fun validate(): Command {
+        return this
+            .makeWords()
+            .oneOrTwoWords()
+            .goWords()
+            .magicWords()
+            .errorIfOnlyOneWord()
+            .findOperation()
+    }
+
+    fun makeWords(): Command {
+        words += input.split(" ")
+        return this
+    }
+
+    fun oneOrTwoWords(): Command {
+        if (words.size < 1 || words.size > 2 ){
+            words.clear()
+            words.add("countError")
+            words.add(input)
+        }
+        return this
+    }
+
+    fun errorIfOnlyOneWord(): Command {
+        if (words.size == 2) return this
+        words.add(0,"verbError")
+        return this
+    }
+
+    fun findOperation(): Command {
+        operation = when (verb) {
+            "take" -> ::take
+            "go" -> ::go
+            "say" -> ::say
+            "verbError" -> ::verbError
+            "countError" -> ::countError
+            else -> ::commandError
+        }
+        return this
+    }
+
+    fun goWords(): Command {
+        val directions = listOf(
+            "n","e","s","w","north","east","south","west",
+            "nw","northwest", "sw","southwest", "ne", "northeast", "se", "southeast",
+            "up","dn","down")
+        return substituteSingle("go", directions)
+    }
+
+    fun magicWords(): Command {
+        val magicWords = listOf("xyzzy", "plugh")
+        return substituteSingle("say", magicWords)
+    }
+
+    fun substituteSingle(sub: String, singles: List<String>): Command {
+        if (words.size == 2) return this
+        if (words[0] in singles) words.add(0, sub)
+        return this
+    }
+
+    // execution
+
+    fun execute(): String {
+        result = operation(noun)
+        return result
+    }
+
+    fun commandError(noun: String) : String = "command error '$input'."
+    fun go(noun: String): String = "went $noun."
+    fun say(noun:String): String = "said $noun."
+    fun take(noun:String): String = "$noun taken."
+    fun verbError(noun: String): String = "I don't understand $noun."
+    fun countError(noun: String): String = "I understand only one- and two-word commands, not '$noun'."
+}
