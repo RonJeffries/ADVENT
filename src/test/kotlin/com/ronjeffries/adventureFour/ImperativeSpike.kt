fe6790aa1a9994e6735a4a1c5dfdcb8950fee094
Ron Jeffries <ronjeffries@acm.org>
2022-09-01 10:38:44 -0400

    rename ImperativeMap to ImperativeFactory

--- src/test/kotlin/com/ronjeffries/adventureFour/ImperativeSpike.kt
+++ src/test/kotlin/com/ronjeffries/adventureFour/ImperativeSpike.kt
@@ -23,7 +23,7 @@ class ImperativeSpike {
 
     @Test
     fun `look up some imperatives`() {
-        val imperatives = ImperativeMap(impTable)
+        val imperatives = ImperativeFactory(impTable)
         var imp: Imperative = imperatives.create("east")
         assertThat(imp).isEqualTo(Imperative("go","east"))
         imp = imperatives.create("e")
@@ -32,7 +32,7 @@ class ImperativeSpike {
 
     @Test
     fun `imperative can act`() {
-        val imperatives = ImperativeMap(impTable)
+        val imperatives = ImperativeFactory(impTable)
         var imp: Imperative = imperatives.create("east")
         assertThat(imp.act()).isEqualTo("went east")
         imp = imperatives.create("e")
@@ -40,16 +40,33 @@ class ImperativeSpike {
         imp = Imperative("forge", "sword")
         assertThat(imp.act()).isEqualTo("I can't forge a sword")
     }
+
+    @Test
+    fun `two word legal command`() {
+        val imperatives = ImperativeFactory(impTable)
+        var imp = imperatives.create("go", "e")
+        assertThat(imp.act()).isEqualTo("went east")
+    }
 }
 
-class ImperativeMap(val map:Map<String,Imperative>, val synonyms:Map<String,String> = synMap) {
-    fun create(verb:String, noun:String = ""): Imperative {
+class ImperativeFactory(val map:Map<String,Imperative>, val synonyms:Map<String,String> = synMap) {
+    fun create(verb:String): Imperative {
         return map.getValue(synonyms.getValue(verb))
+    }
+
+    fun create(verb:String, noun:String): Imperative {
+        val v = synonyms.getValue(verb)
+        val n = synonyms.getValue(noun)
+        val imp = map.getValue(v)
+        return Imperative(imp.verb, n)
     }
 }
 
 val synMap = mapOf("e" to "east", "n" to "north").withDefault { it }
 
-val impTable = mapOf("east" to Imperative("go","east")).withDefault {
+val impTable = mapOf(
+    "east" to Imperative("go","east"),
+    "go" to Imperative("go", "irrelevant")
+    ).withDefault {
     (Imperative("none", "none"))
 }
