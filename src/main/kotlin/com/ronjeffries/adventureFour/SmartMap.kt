a2a581cd4bcb9240450b0742eeb94648c46f3e7c
Ron Jeffries <ronjeffries@acm.org>
2022-09-08 10:33:32 -0400

    Move SmartMap to own file

---
+++ src/main/kotlin/com/ronjeffries/adventureFour/SmartMap.kt
@@ -0,0 +1,7 @@
+package com.ronjeffries.adventureFour
+
+class SmartMap<K,V>(private val global: MutableMap<K,V>, private val local: MutableMap<K,V>) {
+    private val safeLocal = local.withDefault { key: K -> getGlobalValue(key) }
+    fun getValue(key: K): V = safeLocal.getValue(key)
+    fun getGlobalValue(key: K): V = global.getValue(key)
+}
