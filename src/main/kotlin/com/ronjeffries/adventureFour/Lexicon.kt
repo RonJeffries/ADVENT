fa21cc2b67b291ede68b2f241c9150286e170003
Ron Jeffries <ronjeffries@acm.org>
2022-09-15 09:48:47 -0400

    Lexicon.translate returns a phrase, not an imperative.

--- src/main/kotlin/com/ronjeffries/adventureFour/Lexicon.kt
+++ src/main/kotlin/com/ronjeffries/adventureFour/Lexicon.kt
@@ -18,7 +18,7 @@ typealias ActionMap = MutableMap<Phrase, Action>
 
 class Lexicon(private val synonyms: Synonyms, private val verbs: Verbs, val actions: Actions) {
     fun synonym(word:String):String = synonyms.synonym(word)
-    fun translate(word: String): Imperative = verbs.translate(word)
+    fun translate(word: String): Phrase = verbs.translate(word).phrase
 }
 
 class Synonyms(private val map: Map<String,String>) {
