f61c322e70c19bb976b809c509ec77606e1b12d4
Ron Jeffries <ronjeffries@acm.org>
2022-09-16 11:28:19 -0400

    providing "ignored" parameters in GameResponse creations.

--- src/test/kotlin/com/ronjeffries/adventureFour/PlayerResponseTest.kt
+++ src/test/kotlin/com/ronjeffries/adventureFour/PlayerResponseTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 class PlayerResponseTest {
     @Test
     fun `response has say method`() {
-        val r = GameResponse()
+        val r = GameResponse("ignored")
         r.say("One thing")
         r.say("Another thing")
         assertThat(r.sayings).isEqualTo("One thing\nAnother thing\n")
