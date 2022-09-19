50fc6972f3803e8c7b07bb830d62f23aceb02fec
Ron Jeffries <ronjeffries@acm.org>
2022-09-19 15:43:16 -0400

    tidying

--- src/main/kotlin/com/ronjeffries/adventureFour/Rooms.kt
+++ src/main/kotlin/com/ronjeffries/adventureFour/Rooms.kt
@@ -3,25 +3,16 @@ package com.ronjeffries.adventureFour
 class Rooms {
     private val rooms = mutableMapOf<String,Room>()
     val size get() = rooms.size
-    fun names() = rooms.keys
-
-    val roomReferences: Set<String> get() {
-        return (rooms.flatMap { it.value.roomReferences }).toSet()
-    }
 
     fun add(room: Room) {
         rooms[room.roomName] = room
     }
 
-    fun getOrDefault(name: String, default: Room): Room {
-        return rooms.getOrDefault(name, default)
-    }
+    fun containsKey(name: String): Boolean  = rooms.containsKey(name)
 
-    fun containsKey(name: String): Boolean {
-        return rooms.containsKey(name)
-    }
+    fun getOrDefault(name: String, default: Room): Room = rooms.getOrDefault(name, default)
 
-    fun unsafeRoomNamed(name: String): Room {
-        return rooms[name]!!
-    }
+    val roomReferences: Set<String> get() = (rooms.flatMap { it.value.roomReferences }).toSet()
+
+    fun unsafeRoomNamed(name: String): Room  = rooms[name]!!
 }
