eec4d577da87dcff74196ead423572a1b5ab4af0
Ron Jeffries <ronjeffries@acm.org>
2022-10-05 19:58:50 -0400

    tidying. stop me!

--- src/main/kotlin/com/ronjeffries/adventureFour/Facts.kt
+++ src/main/kotlin/com/ronjeffries/adventureFour/Facts.kt
@@ -4,10 +4,10 @@ class Facts {
     private val map = mutableMapOf<String, Fact>()
     operator fun get(name: String) = map.getOrPut(name) { Fact() }
 
-    fun increment(name: String): Int            = this[name].increment()
+    fun increment(name: String): Unit           = this[name].increment()
     fun isFalse(name: String): Boolean          = this[name].isFalse
     fun isTrue(name: String): Boolean           = this[name].isTrue
-    fun decrement(name: String): Int            = this[name].decrement()
+    fun decrement(name: String): Unit           = this[name].decrement()
     fun not(name: String): Unit                 = this[name].not()
     fun set(name: String, truth: Boolean): Unit = this[name].set(truth)
     fun set(name: String, amount: Int): Unit    = this[name].set(amount)
@@ -18,8 +18,8 @@ class Facts {
 class Fact(var value: Int = 0) {
     val isTrue: Boolean get()  = value != 0
     val isFalse: Boolean get() = value == 0
-    fun decrement()            = value--
-    fun increment()            = value++
+    fun decrement()            { value -= 1 }
+    fun increment()            { value += 1 }
     fun not()                  { value = if (isTrue) 0 else 1 }
     fun set(amount:Int)        { value = amount }
     fun set(truth: Boolean)    { value = if (truth) 1 else 0 }
