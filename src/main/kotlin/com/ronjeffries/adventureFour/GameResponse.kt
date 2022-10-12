e2f32a821a7dff3d0776b9240b9f6adc0efa8862
Ron Jeffries <ronjeffries@acm.org>
2022-10-12 15:31:38 -0400

    trying stringbuilder in resultstring

--- src/main/kotlin/com/ronjeffries/adventureFour/GameResponse.kt
+++ src/main/kotlin/com/ronjeffries/adventureFour/GameResponse.kt
@@ -3,7 +3,12 @@ package com.ronjeffries.adventureFour
 class GameResponse(private val player: Player) {
     var nextRoomName = player.currentRoomName
 
-    val resultString: String get() = sayings + nextRoomName.description() + "\n"+ nextRoomName.itemString()
+    val resultString: String get() = buildString {
+        append(sayings)
+        append(nextRoomName.description())
+        append("\n")
+        append(nextRoomName.itemString())
+    }
     var sayings = ""
 
     fun goToPriorRoom() = moveToRoomNamed(player.priorRoomName)
